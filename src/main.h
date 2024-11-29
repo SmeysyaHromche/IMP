49925868c479a5d1e15cda5bd9482ade3f81ff59
SmeysyaHromche <MyrKykhta@gmail.com>
2024-11-29 11:41:47 +0100

    init commit

---
+++ src/main.h
@@ -0,0 +1,26 @@
+/**
+ * @brief Header file of projetc
+ * @author Myron Kukhta (xkukht01) 
+ * 
+ */
+
+
+#include <Wire.h>
+
+// display
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGTH 64
+#define OLED_REST -1
+
+
+/**
+ * @brief First initializations
+ * 
+ */
+void setup();
+
+/**
+ * @brief Behavioral of programm
+ * 
+ */
+void loop();
